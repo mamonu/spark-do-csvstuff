bb63321662a32f4814ba4442ba7746f3fbe9e8d3
mamonu <thorosm@msn.com>
2016-05-19 00:43:34 +0100

    first commit

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "hellospark"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "1.6.1"
+
+    
---
+++ src/main/scala-2.11/SimpleSparkApp.scala
@@ -0,0 +1,33 @@
+/**
+  * Created by mamonu on 19/05/2016.
+  */
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+
+
+
+object SimpleSparkApp {
+
+
+
+
+  def main(args: Array[String]) {
+    val logFile = "/Users/thorosm2002/scripts/Bean.txt" // Should be some file on your system
+    val conf = new SparkConf().setAppName("Simple Application").setMaster("local")
+    val sc = new SparkContext(conf)
+    val logData = sc.textFile(logFile, 2).cache()
+    val numAs = logData.filter(line => line.contains("statistics")).count()
+    val numBs = logData.filter(line => line.contains("data")).count()
+    println("Lines with statistics: %s, Lines with data: %s".format(numAs, numBs))
+  }
+
+
+
+
+
+
+
+
+
+}
