3d46dbd06e47ad82570192c896b54f4763c66b8c
mamonu <thorosm@msn.com>
2016-06-02 15:20:00 +0100

    holy cow..the speed

--- src/main/scala-2.11/SimpleSparkApp.scala
+++ src/main/scala-2.11/SimpleSparkApp.scala
@@ -5,7 +5,7 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 import com.databricks.spark.csv
-
+import org.apache.spark.sql.SQLContext
 
 
 object SimpleSparkApp {
@@ -14,13 +14,43 @@ object SimpleSparkApp {
 
 
   def main(args: Array[String]) {
-    val logFile = "/Users/thorosm2002/scripts/Bean.txt" // Should be some file on your system
-    val conf = new SparkConf().setAppName("Simple Application").setMaster("local")
+    val logFile = "file:///home/mamonu/scripts/hellospark/LCF.csv" // Should be some file on your system
+    val conf = new SparkConf().setAppName("Simple Application").setMaster("local[4]")
     val sc = new SparkContext(conf)
     val logData = sc.textFile(logFile, 2).cache()
-    val numAs = logData.filter(line => line.contains("statistics")).count()
-    val numBs = logData.filter(line => line.contains("data")).count()
-    println("Lines with statistics: %s, Lines with data: %s".format(numAs, numBs))
+    val numAs = logData.filter(line => line.contains("bread")).count()
+    val numBs = logData.filter(line => line.contains("cheese")).count()
+    println("Lines with Hamlet: %s, Lines with Othello %s".format(numAs, numBs))
+
+    val  f = sc.textFile("file:///home/mamonu/scripts/hellospark/LCF.csv")
+
+    val wordCounts = f.flatMap(line => line.split(","))
+      .map(word => (word, 1))
+      .reduceByKey(_ + _, 1)  // 2nd arg configures one task (same as number of partitions)
+      .map(item => item.swap) // interchanges position of entries in each tuple
+      .sortByKey(true, 1) // 1st arg configures ascending sort, 2nd arg configures one task
+      .map(item => item.swap).foreach(println)
+
+    val sqlContext = new SQLContext(sc)
+
+    val df = sqlContext.read
+      .format("com.databricks.spark.csv")
+      .option("header", "true") // Use first line of all files as header
+      .option("inferSchema", "false") // Automatically infer data types
+      .load("file:///home/mamonu/scripts/hellospark/LCF.csv")
+
+
+    df.printSchema()
+
+
+
+    val selectedData = df.select("EXPDESC", "CODING","Paid1","Quantity","Units" )
+    selectedData.write
+      .format("com.databricks.spark.csv")
+      .option("header", "true")
+      .save("file:///home/mamonu/scripts/hellospark/newLCF.csv")
+
+
   }
 
 
